7246db660b2a1a81a70643d7c11e9d7542169c2e
Daniel <daniel.chavez.epet20@gmail.com>
2021-08-07 18:20:24 -0300

    Se cambiaron los nombres de los elementos, y sus clases para poder trabajar con JS; Se añadio el script.js al proyecto para comenzar a trabajar con el lenguaje

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+var header = document.getElementById("header");
+var section_1 = document.getElementById("section_1");
+var section_2 = document.getElementById("section_2");
+var section_3 = document.getElementById("section_3");
+
+header.style.backgroundColor = "#484848"
+
+
+
+
+
+
+
+
+
