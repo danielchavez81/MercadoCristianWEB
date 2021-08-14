32cae301b1db77fa3de11cbd7f0567b5effd50df
Daniel <daniel.chavez.epet20@gmail.com>
2021-08-14 16:44:41 -0300

    Se cambio uno de los colores de la pagina y tambien se modifico el articulo de la seccion 1 para que muestre el texto correctamente y bien alineado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Variables de color
 var color1 = "#DFC84F"
-var color2 = "#484848"
+var color2 = "#080808"
 
 // Referencias a los elementos de HTML
 var header = document.getElementById("header");
@@ -8,11 +8,13 @@ var section_1 = document.getElementById("section_1");
 var section_2 = document.getElementById("section_2");
 var section_3 = document.getElementById("section_3");
 var article_1 = document.getElementById("section_1--Article_1");
+var article_1_Title_Text = document.getElementById("title_Text");
+var article_1_Texts = article_1.querySelectorAll("p");
 var nav_Bar = document.getElementById("nav_Elements");
 
-//Creacion de Elementos HTML
-var article = document.createElement('article');
 
+//Creacion de Elementos HTML
+var article_Section_2 = document.createElement('article');
 
 //Cambios al CSS
 article_1.style.backgroundColor = "transparent";
@@ -26,7 +28,7 @@ nav_Bar.style.height = "148px";
 
 //Articulo Horarios
 
-var section_2_Article_2 = section_2.appendChild(article);
+var section_2_Article_2 = section_2.appendChild(article_Section_2);
 
 section_2.style.position = "relative";
 
@@ -49,14 +51,39 @@ var section_3_Article_1 = section_3.appendChild(numerotelefono);
 header.style.height = "137px";
 header.style.backgroundColor = "";
 header.style.position = "relative";
+header.style.width = "100vw";
 
 nav_Bar.style.height = "137px";
+nav_Bar.style.width = "100vw"
 nav_Bar.style.background = "linear-gradient(#080808, transparent)";
 nav_Bar.style.position = "relative";
 
 section_1.style.position = "absolute";
 section_1.style.top = "0px";
 section_1.style.height = "100vh";
+section_1.style.width = "100vw";
+
+//Se  modifica el  articulo en la seccion 1
+article_1.style.backgroundColor = "transparent";
+article_1.style.borderRadius = "0px";
+article_1.style.border = "";
+article_1.style.alignItems = "flex-start";
+
+for(var i = 0; i < article_1_Texts.length; i++){
+    article_1_Texts[i].style.fontSize = "48px";
+    article_1_Texts[i].style.lineHeight = "30px";
+    article_1_Texts[i].style.color = color2;
+    article_1_Texts[i].style.pointerEvents = "none";
+}
+
+article_1_Title_Text.style.fontSize = "144px";
+article_1_Title_Text.style.lineHeight = "170px"
+article_1_Title_Text.style.color = color2;
+article_1_Title_Text.style.fontWeight = "normal";
+
+
+
+
 
 
 section_2.style.display = "none";
