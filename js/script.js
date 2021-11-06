d0fca9595d63d27d87148d5dd3278449dfc1bb41
Daniel <daniel.chavez.epet20@gmail.com>
2021-11-06 19:54:14 -0300

    Añado la libreria Typed para animar texto, ademas agrego comentarios al codigo JS para una mejor comprension

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 //Variables de color
 const color1 = "#DFC84F";
 const color2 = "#080808";
-
+//Ajustes al body
 document.body.id = "body";
 document.body.style = "display: grid"
 
+//Funcion para crear divs, parametros: id del padre, id propia del div
 function addDiv(IDpadre, id){
     let parent = document.getElementById(IDpadre);
     let element = document.createElement("div");
@@ -13,19 +14,22 @@ function addDiv(IDpadre, id){
     return element;
 }
 
+//Creo los divs padres
 const div1 = addDiv("body", "div1");
 const div2 = addDiv("body", "div2");
 const div3 = addDiv("body", "div3");
 const div4 = addDiv("body", "div4");
 const div5 = addDiv("body", "div5");
 
+//Los coloco dentro de un array
 const divs = [div1, div2, div3,div4,div5]
 
+//Recorro el array y a cada div le asigno la clase "parent_containers" para estilizarlos
 divs.forEach(function(item){
     item.className = "parent_containers";
 })
 
-//NAVBAR
+//Barra de navegacion
 const navbar_element1 = div1.appendChild(document.createElement('a'));navbar_element1.innerHTML = "Inicio"; navbar_element1.href = "#";navbar_element1.style.order = "2"
 const navbar_element2 = div1.appendChild(document.createElement('a'));navbar_element2.innerHTML = "Nosotros"; navbar_element2.href = "#";navbar_element2.style.order = "3"
 const navbar_element3 = div1.appendChild(document.createElement('a'));navbar_element3.innerHTML = "Ubicacion"; navbar_element3.href = "#"; navbar_element3.style.order = "4"
@@ -33,13 +37,12 @@ const navbar_elements = [navbar_element1, navbar_element2, navbar_element3];
 navbar_elements.forEach(function (item) {
     item.className = "navbar_elements"    
 })
+//Logo barra de navegacion
 const logo =  div1.appendChild(document.createElement('img'))
 logo.src = "img/logo1.png";
 logo.id = "logo";
 
-//INICIO
-
-
+//Funcion que hace a la barra de navegacion Sticky
 window.onscroll = function(){f()}
 var sticky = div1.offsetTop;
 function f(){
@@ -50,6 +53,7 @@ function f(){
     }
 }
 
+//Animacion doble click sobre el logo
 var logo_anim = anime({
     targets: '#logo',
     keyframes:[
@@ -65,3 +69,13 @@ var logo_anim = anime({
 
 logo.ondblclick = logo_anim.play;
 
+//Texto animado al cargar la pagina
+let test = addDiv("div2", "test");
+test.style = "width: 400px; height: 200px;"
+
+var options = {
+    strings: ['<i class="title_text">Mercado Cristian</i>', '<i class="title_text">Tu mercado de confianza</i>'],
+    typeSpeed: 4,
+    smartBackspace: true    
+};
+const titulo = new Typed('#test', options);
